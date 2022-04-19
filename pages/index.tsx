2324252d5690e693b372ec4ab69a5a81cef527c2
Adhiana Mastur <adhianamastur48@gmail.com>
2022-04-19 21:21:43 +0700

    Update FeaturedPosts, Category Page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import { PostCard, PostWidget, Categories } from '../components';
+import { FeaturedPosts } from '../sections';
 import { getPosts } from '../services';
 
 
@@ -13,6 +14,7 @@ const Home: NextPage = ({ posts }) => {
         <title>Headless CMS Blog</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <FeaturedPosts />
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
         <div className="lg:col-span-8 col-span-1">
           {posts.map((post, index) => (
